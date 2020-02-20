69fa334e78cbee853bf468c9ecd8d9ef284a4cea
Antoine GÉLIN <gelinantoine@gmail.com>
2020-02-20 14:05:51 -0500

    c + detect doublons

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 
 
@@ -36,22 +37,47 @@ int main(){
 
 
 
+	// //Test
+	// printf("%d %d %d\n",B,L,D);
+	// for(auto score : scores){
+	// 	printf("%d ",score);
+	// }
+	// printf("\n");
+	// for(int i=0; i<L;++i){
+	// 	printf("%d %d %d\n",libs[i][0],libs[i][1],libs[i][2]);
+	// 	for(int j=0;j<libs[i][0];++j){
+	// 		printf("%d ",books[i][j]);
+	// 	}
+	// 	printf("\n");
+	// }
+
+	
 
 
+	//naif
+	// classer tab par metric
+	vector<int> dejaFaits ;
 
-	//Test
-	printf("%d %d %d\n",B,L,D);
-	for(auto score : scores){
-		printf("%d ",score);
-	}
-	printf("\n");
-	for(int i=0; i<L;++i){
-		printf("%d %d %d\n",libs[i][0],libs[i][1],libs[i][2]);
+	printf("%d\n",L);
+	for(int i=0; i<L; ++i){
+		// printf("%d %d \n",i,libs[i][0]);
+		vector<int> livresAScan ;
 		for(int j=0;j<libs[i][0];++j){
-			printf("%d ",books[i][j]);
+			
+			
+			if  (! ( std::find(dejaFaits.begin(), dejaFaits.end(), books[i][j]) != dejaFaits.end()) ){
+				dejaFaits.push_back(books[i][j]);
+				livresAScan.push_back(books[i][j]);
+			}
+
+		}
+		cout << i << " " << livresAScan.size()<<std::endl;
+		for (std::vector<int>::const_iterator i = livresAScan.begin(); i != livresAScan.end(); ++i){
+    		std::cout << *i << ' ';
 		}
 		printf("\n");
 	}
+
 
 
 	return 0;
